b7f6fec5becece08d77d9d80838c25250793a9b1
Ammar Rudani <aarudani7@gmail.com>
2019-10-09 21:40:14 -0400

    added both datasets

--- ML-proj2.py
+++ ML-proj2.py
@@ -2,13 +2,19 @@ import pandas as pd
 import numpy as np
 import numbers
 import decimal
-import logisticRegression as lR
-import linearDiscriminantAnalysis as lDA
+# import logisticRegression as lR
+# import linearDiscriminantAnalysis as lDA
 import scipy.stats as ss
 import matplotlib.pyplot as plt
 from statistics import stdev
 from statistics import mean
 import time
 import math
+
+
+
+redditData = pd.read_csv("reddit_test.csv") 
+
+# print(redditData)
 
 
