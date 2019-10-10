65b19ddb58dff38cc59b43dbb109ee27656795a0
Ammar Rudani <aarudani7@gmail.com>
2019-10-09 20:39:25 -0400

    Initial Commit

---
+++ ML-proj2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import numbers
+import decimal
+import logisticRegression as lR
+import linearDiscriminantAnalysis as lDA
+import scipy.stats as ss
+import matplotlib.pyplot as plt
+from statistics import stdev
+from statistics import mean
+import time
+import math
+
+
